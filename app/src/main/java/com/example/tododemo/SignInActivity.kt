42a68d34fad9656a750b619f985e52d4268e55a6
AhoiJ <j.aho@luukku.com>
2019-10-11 17:03:52 +0300

    Login activity UI done

---
+++ app/src/main/java/com/example/tododemo/SignInActivity.kt
@@ -0,0 +1,39 @@
+package com.example.tododemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_sign_in.*
+
+class SignInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+
+        btnSignUp.setOnClickListener(View.OnClickListener {
+
+            signUp()
+        })
+
+        btnLogin.setOnClickListener(View.OnClickListener {
+
+
+            login()
+        })
+
+    }
+
+
+    fun signUp() {
+
+
+    }
+
+    fun login() {
+
+
+    }
+
+}
