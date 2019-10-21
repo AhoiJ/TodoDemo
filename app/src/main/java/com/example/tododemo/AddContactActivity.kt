7c4d9c059ec949d429f36226ae2358be5491bf16
Jenni Tammenmaa <jenni.tammenmaa@eng.tamk.fi>
2019-10-21 13:56:33 +0300

    add contact added, no constraints on AddContactActivity

---
+++ app/src/main/java/com/example/tododemo/AddContactActivity.kt
@@ -0,0 +1,18 @@
+package com.example.tododemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+
+class AddContactActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_contact)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+}
