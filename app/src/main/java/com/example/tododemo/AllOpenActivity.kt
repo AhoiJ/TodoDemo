0c6d9b59e74bc49388956ece776689d6ad51528f
Jere <j.aho@luukku.com>
2019-11-04 22:23:17 +0200

    Added comments and tidied up the code

--- app/src/main/java/com/example/tododemo/AllOpenActivity.kt
+++ app/src/main/java/com/example/tododemo/AllOpenActivity.kt
@@ -11,6 +11,7 @@ import kotlinx.android.synthetic.main.activity_all_open.*
 
 class AllOpenActivity : AppCompatActivity() {
 
+    // initialize list to take data from db
     private var toDo: MutableList<ToDoList> = mutableListOf()
 
     private lateinit var auth: FirebaseAuth
@@ -32,15 +33,16 @@ class AllOpenActivity : AppCompatActivity() {
 
         val firebaseUser = auth.currentUser // currently useless, should be in onStart
         val userID = firebaseUser!!.uid
+        // this is where the magic happens
         initToDoList()
-
-
 
     }
 
 
     private fun initToDoList() {
+        // gets snapchot of DB data
         val todoListener = object : ValueEventListener {
+            // refuses to work unless using mutableList<ToDoList>
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 dataSnapshot.children.mapNotNullTo(toDo){
                     it.getValue<ToDoList>(ToDoList::class.java)
@@ -55,43 +57,23 @@ class AllOpenActivity : AppCompatActivity() {
         }
         _db.child("ToDos").addValueEventListener(todoListener)
     }
-
+    // correctly displays to-do list titles but otherwise useless
     private fun updateNakyma(lista: MutableList<ToDoList>){
-       // tvTitle.setText(lista.title)
+        // initialize list
         var listaus: List<String?> = mutableListOf()
-
+        // initialize variable of selfmade class ToDoList
         var too = ToDoList()
-
+        // goes through all elements in lista, 'too' can only hold [1] object at a time
         for (i in 0..lista.lastIndex ){
             too = lista[i]
-            val titteli: String? = too.title
-
-            listaus += titteli
+            // transfer title from too to a mutable list(i know it makes no sense)
+            listaus += too.title
         }
-
-
-
-
+        // create listview using custom layout item
         val adapter = ArrayAdapter<String>(this, R.layout.listview_item, listaus)
         val listView: ListView = findViewById(R.id.listview_1)
         listView.setAdapter(adapter)
-
-
-/*
-        val listView: ListView = findViewById(R.id.listview_1)
-        listView.setAdapter(adapter)
-
-         */
+        
     }
 
 }
-
-
-/*
-    val adapter = ArrayAdapter(this,
-            R.layout.listview_item, array)
-
-        val listView: ListView = findViewById(R.id.listview_1)
-        listView.setAdapter(adapter)
-
- */
