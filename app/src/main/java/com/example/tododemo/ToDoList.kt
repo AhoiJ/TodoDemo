b3a3a3c2f4a3e9a381e34de98e202b06780fceae
Jere Aho <45847630+AhoiJ@users.noreply.github.com>
2019-11-05 00:29:23 +0200

    Update ToDoList.kt

--- app/src/main/java/com/example/tododemo/ToDoList.kt
+++ app/src/main/java/com/example/tododemo/ToDoList.kt
@@ -6,6 +6,7 @@ class ToDoList {
     }
     var objId: String? = null
     var title: String? = null
+    // done boolean should be on each task, may need to implement "subObject" 
     var tasks: List<String>? = null
     var done: Boolean? = false
     var startTime: String? = null
