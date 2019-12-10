812f95f85dd74a0d49ce9dd55c5aaf1207d875d6
Jenni Tammenmaa <jenni.tammenmaa@eng.tamk.fi>
2019-12-10 14:46:25 +0200

    SingleTodo näyttää titlen ja subtodot. Nappi lisätty, jotta kaverin voi kutsua. Nappi testattu.

--- app/src/main/java/com/example/tododemo/SingleTodo.kt
+++ app/src/main/java/com/example/tododemo/SingleTodo.kt
@@ -3,11 +3,9 @@ package com.example.tododemo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.ListAdapter
+import android.view.View
+import android.widget.*
 import android.widget.AdapterView.OnItemClickListener
-import android.widget.ListView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_single_todo.*
 import java.io.Serializable
@@ -24,8 +22,14 @@ class SingleTodo : AppCompatActivity() {
         singleTodo = intent.extras!!.get("toDoList") as ToDoList
         populateList(singleTodo)
         //hakee titlen tekstimuodossa
-        //textView.setText(singleTodo.title)
+        textTitle.setText(singleTodo.title)
         //items_list.setText(singleTodo.tasks)
+
+        //button for inviting friends to share todos
+        //testattu, että tulostaa Hello Worldin konsoliin
+        buttonInvite.setOnClickListener(View.OnClickListener {
+           println("Hello World!")
+        })
     }
 
     override fun finish() {
